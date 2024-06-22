740cad546250c7d7eb66a8917efe87af2bdd97a5
mudar-hussain <mudar.shussain@gmail.com>
2024-06-22 20:45:44 +0530

    Firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, importProvidersFrom } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment.prod';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +19,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+      provideFirestore(() => getFirestore()),
+      provideStorage(() => getStorage()),
+      provideAuth(() => getAuth())
+    )],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
